091f61db30c9e4153b046fff9844db97b6a85e3d
nvminhtam <nvminhtam2201@gmail.com>
2022-01-02 18:14:53 +0700

    prebooking layout

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // use routes
-app.use('/', indexRouter);
+app.use('/', prebookingRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
