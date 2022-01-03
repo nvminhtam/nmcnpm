eb7584ae997d6d3efacbf965e42f71b79a4ea2be
Minh Tam <nvminhtam2201@gmail.com>
2022-01-03 08:19:38 +0700

    update layout

--- models/index.js
+++ models/index.js
@@ -3,10 +3,7 @@ const initModels = require("./init-models");
 
 // const sequelize = new Sequelize(process.env.DATABASE_URL);
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
-});
+const sequelize = new Sequelize("mysql://doadmin:07TZPIrkKqJrHQ0Q@db-mysql-sgp1-23535-do-user-9572625-0.b.db.ondigitalocean.com:25060/NMCNPM?ssl-mode=REQUIRED");
 
 module.exports = {
     sequelize,
