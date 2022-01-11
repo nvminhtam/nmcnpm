d28e1f77476ada87a7e882291e6252a882a50b9b
nvminhtam <nvminhtam2201@gmail.com>
2022-01-12 00:39:04 +0700

    edit header+footer, add bill to db

--- models/index.js
+++ models/index.js
@@ -1,35 +1,16 @@
 const Sequelize = require('sequelize');
 const initModels = require("./init-models");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialectOptions: {
-        useUTC: false, //for reading from database
-        dateStrings: true,
-        typeCast: function(field, next) { // for reading from database
-            if (field.type === 'DATETIME') {
-                return field.string()
-            }
-            return next()
-        },
-    },
-    timezone: '+07:00'
-});
-
-// const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-//     host: process.env.DB_HOST,
-//     dialect: 'mysql',
-//     dialectOptions: {
-//         useUTC: false, //for reading from database
-//         dateStrings: true,
-//         typeCast: function(field, next) { // for reading from database
-//             if (field.type === 'DATETIME') {
-//                 return field.string()
-//             }
-//             return next()
-//         },
-//     },
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//  
 //     timezone: '+07:00'
 // });
+
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    timezone: '+07:00'
+});
 
 module.exports = {
     sequelize,
