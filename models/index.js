6b4d0cd14f080d0bb7a8c3db7c7f9158ec280aee
Minh Tam <nvminhtam2201@gmail.com>
2022-01-12 20:44:37 +0700

    update db connection

--- models/index.js
+++ models/index.js
@@ -1,16 +1,16 @@
 const Sequelize = require('sequelize');
 const initModels = require("./init-models");
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//  
-//     timezone: '+07:00'
-// });
-
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+ 
     timezone: '+07:00'
 });
+
+// const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',
+//     timezone: '+07:00'
+// });
 
 module.exports = {
     sequelize,
